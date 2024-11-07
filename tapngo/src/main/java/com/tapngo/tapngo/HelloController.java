228eb23d551276148563e7d637be57426d594728
Ali-Shalby <ali.shalby03@gmail.com>
2024-11-07 15:59:16 +0100

    prova test

--- tapngo/src/main/java/com/tapngo/tapngo/HelloController.java
+++ tapngo/src/main/java/com/tapngo/tapngo/HelloController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private int ciccio;
 
     @FXML
     protected void onHelloButtonClick() {
