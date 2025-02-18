ebe8c9ab724f2996376a2dc12e8c9e2292f68b75
Ali-Shalby <ali.shalby03@gmail.com>
2025-02-18 16:28:12 +0100

    aggiornamento 18/02/2025

--- tapngo/src/main/java/com/tapngo/view/LoginGrafico2.java
+++ tapngo/src/main/java/com/tapngo/view/LoginGrafico2.java
@@ -33,21 +33,21 @@ public class LoginGrafico2 {
     protected void onLoginButtonClick() throws IOException {
 
         // Gestione eccezione per validità email
-        CredentialsBean credB = null;
+        CredentialsBean credB2 = null;
         try {
-            credB = new CredentialsBean(textFieldUsername.getText(), textFieldPassword.getText());
+            credB2 = new CredentialsBean(textFieldUsername.getText(), textFieldPassword.getText());
         } catch (IllegalArgumentException e) {
             Popup.mostraPopup("Errore email", "L'email fornita non è valida!", ERROR_POPUP_TYPE);
             loginView();
         }
 
         // Verifica se credB è stato istanziato correttamente
-        if (credB != null) {
+        if (credB2 != null) {
             LoginController loginController = new LoginController();
 
             // Chiamata al login controller per effettuare il login
             try {
-                loginController.start(credB);
+                loginController.start(credB2);
 
 
                 // Controlla il ruolo dell'utente e carica la view appropriata
@@ -57,7 +57,7 @@ public class LoginGrafico2 {
             } catch (LoadException e) {
                 Popup.mostraPopup("Caricamento non riuscito", "Impossibile caricamento della pagina!", ERROR_POPUP_TYPE);
                 loginView();
-            } catch (DAOException | SQLException | IOException e) {
+            } catch (SQLException | DAOException |  IOException e) {
                 Popup.mostraPopup("Errore", "Hai sbagliato username o password, per favore ricontrolla!", ERROR_POPUP_TYPE);
                 loginView();
             }
@@ -67,38 +67,38 @@ public class LoginGrafico2 {
     // Metodo che gestisce il caricamento della view in base al ruolo
     private void cambiaViewDopoLogin() throws IOException, DAOException, SQLException {
 
-        String fxmlFile;
-        Stage stageChangeView = ApplicazioneStage.getStage();
+        String fxmlFile1;
+        Stage stageChangeView1 = ApplicazioneStage.getStage();
         FXMLLoader fxmlLoader = new FXMLLoader();
 
         if (Credentials.getRole().getId() == 1) {
-            fxmlFile = "/com/tapngo/clienteView2.fxml"; // View per cliente
+            fxmlFile1 = "/com/tapngo/clienteView2.fxml"; // View per cliente
         } else {
-            fxmlFile = "/com/tapngo/ristoratoreView2.fxml"; // View per ristoratore
+            fxmlFile1 = "/com/tapngo/ristoratoreView2.fxml"; // View per ristoratore
         }
 
         // Carica l'FXML
-        fxmlLoader.setLocation(getClass().getResource(fxmlFile));
+        fxmlLoader.setLocation(getClass().getResource(fxmlFile1));
         Parent rootNode = fxmlLoader.load();
 
         // Ottieni il controller associato
         Object controller = fxmlLoader.getController();
 
 
-        if (controller instanceof ClienteControllerGrafico2 clienteControllerGrafico) { // Se il controller è per il cliente
-            clienteControllerGrafico.setLabelTitle("                Ciao " + Credentials.getNome() + ",\n   cosa vuoi mangiare oggi?");
-        } else if (controller instanceof RistoratoreControllerGrafico ristoratoreControllerGrafico) { // Se il controller è per il ristoratore
-            ristoratoreControllerGrafico.setLabelTitle("                  Ciao " + Credentials.getNome() + ",\n    cosa vuoi gestire oggi?");
+        if (controller instanceof ClienteControllerGrafico2 clienteControllerGrafico2) { // Se il controller è per il cliente
+            clienteControllerGrafico2.setLabelTitle("                Ciao " + Credentials.getNome() + ",\n   cosa vuoi mangiare oggi?");
+        } else if (controller instanceof RistoratoreControllerGrafico ristoratoreControllerGrafico2) { // Se il controller è per il ristoratore
+            ristoratoreControllerGrafico2.setLabelTitle("                  Ciao " + Credentials.getNome() + ",\n    cosa vuoi gestire oggi?");
         }
 
         // Imposta la scena
         Scene scene = new Scene(rootNode, ScreenSize.getSceneWidth(), ScreenSize.getSceneHeight());
-        stageChangeView.setTitle("Tap&go");
+        stageChangeView1.setTitle("Tap&go");
         if (controller instanceof ClienteControllerGrafico clienteControllerGrafico) { // Se il controller è per il cliente
             clienteControllerGrafico.setUsername(Credentials.getUsername());}
-        stageChangeView.setScene(scene);
-        stageChangeView.centerOnScreen();
-        stageChangeView.show();
+        stageChangeView1.setScene(scene);
+        stageChangeView1.centerOnScreen();
+        stageChangeView1.show();
     }
 
     @FXML // Metodo per il login
@@ -109,14 +109,14 @@ public class LoginGrafico2 {
         Parent root = fxmlLoader.load(getClass().getResourceAsStream("/com/tapngo/login2.fxml"));
 
         // Ottieni lo stage attuale dalla classe ApplicazioneStage
-        Stage stage = ApplicazioneStage.getStage();
+        Stage stage1 = ApplicazioneStage.getStage();
 
         // Imposta la nuova scena con il layout caricato
         Scene scene = new Scene(root, ScreenSize.getSceneWidth(), ScreenSize.getSceneHeight());
 
         // Cambia la scena dello stage
-        stage.setScene(scene);
-        stage.show();
+        stage1.setScene(scene);
+        stage1.show();
     }
     // Chiamata al controller della registrazione
     public void registratiView() throws IOException {
@@ -130,10 +130,10 @@ public class LoginGrafico2 {
         Parent root = fxmlLoader.load(getClass().getResourceAsStream("/com/tapngo/registratiView2.fxml"));
 
         // Imposta la nuova scena con il layout caricato
-        Scene scene = new Scene(root, ScreenSize.getSceneWidth(), ScreenSize.getSceneHeight());
+        Scene scene1 = new Scene(root, ScreenSize.getSceneWidth(), ScreenSize.getSceneHeight());
 
         // Cambia la scena dello stage
-        stage.setScene(scene);
+        stage.setScene(scene1);
         stage.show();
     }
 
@@ -143,25 +143,25 @@ public class LoginGrafico2 {
 
         // Settaggi scena
         FXMLLoader fxmlLoader;
-        Stage stage = ApplicazioneStage.getStage();
-        Scene scene;
-        String fxmlFile;
+        Stage stage1 = ApplicazioneStage.getStage();
+        Scene scene1;
+        String fxmlFile1;
 
         // Caricamento del login in base alla GUI
         if (ScreenSize.getGUI() == 0){
-            fxmlFile = "/com/tapngo/login.fxml";
+            fxmlFile1 = "/com/tapngo/login.fxml";
         } else {
-            fxmlFile = "/com/tapngo/login2.fxml";
+            fxmlFile1 = "/com/tapngo/login2.fxml";
         }
 
         // Caricamento della scena
         fxmlLoader = new FXMLLoader();
-        Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream(fxmlFile));
-        scene = new Scene(rootNode, ScreenSize.getSceneWidth(), ScreenSize.getSceneHeight());
-        stage.setTitle("Tap&go");
-        stage.setScene(scene);
-        stage.centerOnScreen();
-        stage.show();
+        Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream(fxmlFile1));
+        scene1 = new Scene(rootNode, ScreenSize.getSceneWidth(), ScreenSize.getSceneHeight());
+        stage1.setTitle("Tap&go");
+        stage1.setScene(scene1);
+        stage1.centerOnScreen();
+        stage1.show();
     }
 
 }
